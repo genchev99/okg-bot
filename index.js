d5b17192d2b4ca487326fbd7f72929253ce5a590
Hristiyan Genchev <genchev99@users.noreply.github.com>
2020-01-05 23:57:22 +0200

    Login functionality

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const {Cluster} = require('puppeteer-cluster');
         puppeteer: puppeteer,
     });
 
+    /* Logs into moodle */
+    cluster.queue(async ({page}) => {
+        await page.goto('https://learn.fmi.uni-sofia.bg/login/index.php');
+
+        await page.type('#username', process.env.username);
+        await page.type('#password', process.env.password);
+
+        await page.click('#logintn', {delay: 25});
+
+        await page.waitForNavigation();
+    });
+
     await cluster.idle();
     await cluster.close();
 })();
