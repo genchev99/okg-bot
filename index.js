0c0aa2ef5b9f40c505e0aaaa974fbe68ab056c46
Hristiyan Genchev <genchev99@users.noreply.github.com>
2020-01-24 21:48:25 +0200

    Getting rid of the cluster and add req interceptors

--- index.js
+++ index.js
@@ -3,89 +3,85 @@ const puppeteer = require('puppeteer');
 const {Cluster} = require('puppeteer-cluster');
 
 /* Validation of credentials */
-if (!process.env.USERNAME) {
+if (!process.env.MOODLE_USERNAME) {
     console.error('Please provide a proper username in the .env file');
     process.exit(1);
 }
 
-if (!process.env.PASSWORD) {
+if (!process.env.MOODLE_PASSWORD) {
     console.error('Please provide a proper password in the .env file');
     process.exit(1);
 }
 
 (async () => {
-    const cluster = await Cluster.launch({
-        concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: parseInt(process.env.CONCURRENCY) || 5,
-        puppeteer: puppeteer,
-        timeout: 60000, /* 60 to timeout */
-        puppeteerOptions: {
-            ignoreHTTPSErrors: true,
-            headless: !!parseInt(process.env.headless),
-        },
-    });
+    const browser = await puppeteer.launch({headless: false, ignoreHTTPSErrors: true});
+    const page = await browser.newPage();
 
-    /* Logs into moodle */
-    cluster.queue(async ({page}) => {
-        await page.goto('https://learn.fmi.uni-sofia.bg/login/index.php');
+    await page.setRequestInterception(true);
+    page.on('request', request => {
+        if (request.url().endsWith('recorder.php') && request.method() === 'POST') {
+            const body = request.postData();
 
-        await page.type('#username', process.env.USERNAME);
-        await page.type('#password', process.env.PASSWORD);
-
-        await page.click('#logintn', {delay: 25});
-
-        await page.waitForNavigation();
-    });
-
-    cluster.task(async ({page, data: url}) => {
-        await page.setRequestInterception(true);
-
-        page.on('request', request => {
-            if (request.url().endsWith('request.php') && request.method() === 'POST') {
-                console.log(request.url());
-                console.log(request.postData());
-
-                const realData = JSON.parse(request.postData());
-                //
-                // const postData = JSON.stringify({
-                //     offset: 13,
-                // }).trim();
-                //
-                // // const postData = `offset=${realData.offset}`;
-                // console.log('my post data: ', postData);
-                // console.log(request);
-                //
-                // request.continue({
-                //     'postData': postData,
-                // });
-
+            if (body.indexOf('START') !== -1) {
                 request.continue();
+                return;
             }
 
+            const ACTION = getValue(body, '"ACTION"');
+            const MODEL = getValue(body,'"MODEL"');
+            const DIFFICULTY = getValue(body,'"DIFFICULTY"');
+            const MAX_SCORE = getValue(body,'"MAX_SCORE"');
+            const SCORE = getValue(body,'"SCORE"');
+            const TIME = getValue(body,'"TIME"');
+            const CLICKS = getValue(body,'"CLICKS"');
+            const COMPETENCES = getValue(body,'"COMPETENCES"');
+
+            console.log({
+                ACTION,
+                MODEL,
+                DIFFICULTY,
+                MAX_SCORE,
+                SCORE,
+                TIME,
+                CLICKS,
+                COMPETENCES,
+            });
+
+            console.log(request.headers());
+
+            request.continue({headers: request.headers(), postData: `ACTION=${ACTION}&MODEL=${MODEL}&DIFFICULTY=${DIFFICULTY}&MAX_SCORE=${MAX_SCORE}&SCORE=${MAX_SCORE}&TIME=81798&CLICKS=81798&COMPETENCES=${COMPETENCES}`});
+        } else {
             request.continue();
-        });
-
-        await page.goto(url);
-
-        await page.waitFor('[corner=topLeft]');
-        await page.click('[corner=topLeft]');
-
-        await page.waitFor('#infoPanel');
-        await page.click('#infoPanel');
-
-        await page.close();
-
-        process.exit(1)
+        }
     });
 
-    for (let model = 1; model <= 9; model++) {
+    await page.goto('https://learn.fmi.uni-sofia.bg/login/index.php');
+
+    await page.type('#username', process.env.MOODLE_USERNAME, {delay: 25});
+    await page.type('#password', process.env.MOODLE_PASSWORD, {delay: 25});
+
+    await page.click('#loginbtn', {delay: 25});
+
+    await page.waitFor(4000);
+
+    for (let model = 9; model <= 9; model++) {
         for (let difficulty = 0; difficulty < 3; difficulty++) {
-            for (let times = 0; times < 5; times++) {
-                cluster.queue(`http://pavel.it.fmi.uni-sofia.bg/courses/okg/tests/model.html?models=t00${model}&difficulty=${difficulty}`)
+            for (let times = 0; times < 20; times++) {
+                const url = `http://pavel.it.fmi.uni-sofia.bg/courses/okg/tests/model.html?models=t00${model}&difficulty=${difficulty}`;
+                console.log('opening: ', url);
+                await page.goto(url, {timeout: 200000});
+
+                await page.waitFor('[corner=topLeft]');
+                await page.click('[corner=topLeft]');
+
+                await page.waitFor(1000);
+                await page.waitFor('#infoPanel');
+                await page.click('#infoPanel');
             }
         }
     }
-
-    await cluster.idle();
-    await cluster.close();
 })();
+
+const getValue = (body, string) => {
+    return (body.slice(body.indexOf(string) + string.length, body.length)).split('\r\n')[2];
+};
